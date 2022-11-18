32369868c60820b18249ddfd68f819c422371145
RominaMalimban <romina.lyn@hotmail.it>
2022-11-18 14:49:51 +0100

    impostazione timer (da cambiare i secondi)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// variabili manipolazione DOM:
+let listNum = document.querySelector(".numbers");
 
 // creo lista di 5 numeri random servendomi della funzione numRandom e arrayRandom:
 let myArray = arrayRandom (5, 1, 100);
 console.log(myArray);
+listNum.innerHTML = myArray;
+
+// imposto timer di secondi e faccio scomparire la lista di numeri:
+setTimeout(function(){
+    listNum.style.display = "none";
+}, 2000);
