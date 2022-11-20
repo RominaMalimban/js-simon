f2b824fdd16070ab75fe04f5881d1075c86067e6
RominaMalimban <romina.lyn@hotmail.it>
2022-11-20 11:48:37 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -6,75 +6,70 @@
 // variabili manipolazione DOM:
 let listNum = document.querySelector(".numbers");
 let timer = document.getElementById("timer");
-let indovina = document.getElementById("indovina");
 let button = document.querySelector("button");
 let input = document.getElementById("my-num");
 
 // creo lista di 5 numeri random servendomi della funzione numRandom e arrayRandom:
 let arrayNumRandom = arrayRandom(5, 1, 100);
-console.log(arrayNumRandom);
 listNum.innerHTML = arrayNumRandom;
 
 // creo array vuoto che conterrà i numeri che inserirà l'utente:
 let myArray = [];
 
 // imposto timer di secondi:
-let seconds = 2;
+let seconds = 30;
 
 // imposto il timer tramite il setInterval:
 let clock = setInterval(function(){
-    console.log(seconds);
 
     if(seconds === -1){
         clearInterval(clock);
         listNum.style.display = "none";
         timer.style.display = "none";
-        indovina.style.display = "block";
-        document.querySelector("#inputs").style.display = "block"
+        document.querySelector("h1").innerHTML = `Ora inserisci i numeri che hai memorizzato`;
+        document.querySelector("#inputs").style.display = "block";
 
-        
+        // creo due variabili in cui salvo due array vuoti in cui inserisco i numeri corretti e quelli sbagliati:
         let numGiusti = [];
         let numSbagliati = [];
-        // click:
+
+        // al click pusho i numeri inseriti da utente in array:
         button.addEventListener("click", 
             function(){
+
                 let selections = parseInt(input.value);
                 input.value = "";
                 
                 myArray.push(selections);
-                console.log("Array numeri selezionati da utente" , myArray);
 
-                // condizione: dopo 5 click faccio sparire l'input:
+                // condizione: se utente inserisce 5 numeri sparisce l'input:
                 if(myArray.length === 5){
                     input.style.display = "none";
                     button.style.display = "none";
                 }
                 
-                // condizione: se i numeri inseriti da utente combaciano con quelli random li metto in un altro array e metto a schermo il punteggio:
+                // condizione: se i numeri inseriti da utente combaciano con quelli random li metto in array numGiusti, altrimenti in quello dei numSbagliati e metto a schermo il punteggio:
                 if(arrayNumRandom.includes(selections)){
                     
                     numGiusti.push(selections);
-                    console.log("Array numeri giusti: ", numGiusti)
                 
                     let score = numGiusti.length;
-                    document.querySelector("h2").innerHTML = `PUNTEGGIO: ${score} punti. Hai indovinato il numero:${numGiusti}`
+                    document.querySelector("#score").innerHTML = `PUNTEGGIO: ${score} punti. Hai indovinato: ${numGiusti}`;
 
                 }else{
                     numSbagliati.push(selections);
-                    document.querySelector("h3").innerHTML = `Ops! Numeri sbagliati ${numSbagliati}.`
-                }
+                    document.querySelector("#error").innerHTML = `Ops! Numeri sbagliati da te inseriti: ${numSbagliati}.`;
+
+                    if(numSbagliati.length === 5){
+                        document.querySelector("#zero").innerHTML = `Complimenti non hai azzeccato neanche un numero! Hai forse dei problemi di memoria?`;
+                    }
+                };
             }
         );
 
 
     }else{
         timer.innerHTML = seconds--;
-    }
+    };
 
-}, 1000)
-
-
-
-
-   
-
+}, 1000);
